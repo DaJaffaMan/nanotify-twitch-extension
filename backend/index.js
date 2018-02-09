446c46d270cb5007cfd18c905748e554a4401ed6
Jack Jefferies <jack@gamebench.net>
2018-02-09 17:36:46 +0000

    adding websocket and displaying block count from node

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,50 @@
 "use strict";
 
-const { default: Rai } = require('raiblocks-js');
+const {default: Rai} = require('raiblocks-js');
 const express = require('express');
 const fs = require('fs');
 const https = require('https');
+const WS = require('ws');
+
+const options = {
+	key: fs.readFileSync('/boilerplate/certs/testing.key'),
+	cert: fs.readFileSync('/boilerplate/certs/testing.crt')
+};
+
+const PORT = 9001;
 
 const app = express();
-const rai = new Rai('xrb_38s5duq85sjm59nonxantc94r4k4ogewx86a1dibjc9q64eganu3s9acztko');
+const server = https.createServer(options, app);
+const wss = new WS.Server({server});
+wss.on('connection', function connection(ws) {
+	ws.on('message', function message(msg) {
+		console.log(msg);
+	});
+});
+
+const rai = new Rai('http://ec2-18-196-194-4.eu-central-1.compute.amazonaws.com:7076');
 
 app.use((req, res, next) => {
-  console.log('Got request', req.path, req.method);
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  return next();
+	console.log('Got request', req.path, req.method);
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	return next();
 });
 
 app.use(express.static('../frontend'));
 
-app.get('/blocks', async (req, res) => {
-    const numblocks = await rai.blocks.count();
-    console.log(numblocks)
-	return res.json({blocks: numblocks});
-});
+server.listen(PORT, function () {
+	console.log('Extension Boilerplate service running on https', PORT);
 
-let options = {
-   key  : fs.readFileSync('/boilerplate/certs/testing.key'),
-   cert : fs.readFileSync('/boilerplate/certs/testing.crt')
-};
+	const ws = new WS(`wss://localhost:${server.address().port}`, {
+		rejectUnauthorized: false
+	});
 
-const PORT = 8080;
-https.createServer(options, app).listen(PORT, function () {
-  console.log('Extension Boilerplate service running on https', PORT);
+
+	ws.on('open', function open() {
+		rai.blocks.count().then(res => {
+			console.log(res)
+		});
+	});
 });
