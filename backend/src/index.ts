da1198f4661c3bf863ab3c81ce194c73f7ffe8de
Jack Jefferies <jack@gamebench.net>
2018-02-12 13:54:31 +0000

    request pending transactions every 5 seconds

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import Rai from 'raiblocks-js';
 import * as fs from 'fs';
 import * as https from 'https';
-import * as WS from 'ws';
+import * as WebSocket from 'ws';
 
 const options = {
 	key: fs.readFileSync('/boilerplate/certs/testing.key'),
@@ -13,6 +13,7 @@ const rai = new Rai('http://ec2-18-196-194-4.eu-central-1.compute.amazonaws.com:
 const express = require('express');
 const app = express();
 const server = https.createServer(options, app);
+const wss = new WebSocket.Server({server});
 
 function setRoutes() {
 	app.use((req: any, res: any, next: any) => {
@@ -26,29 +27,29 @@ function setRoutes() {
 	app.use(express.static('../frontend'));
 }
 
-function startWS() {
-	const wss = new WS.Server({server});
+async function startWS() {
 	wss.on('connection', function connection(ws: any) {
 		ws.on('message', function message(msg: any) {
-			console.log(msg);
+			console.log(msg)
 		});
 	});
 
-	const ws = new WS(`wss://localhost:${server.address().port}`, {
+	setInterval(requestPending, 5000);
+}
+
+async function requestPending() {
+	console.log(new Date());
+	const pendingTransactions = await rai.pending.get({account: '',count: 10000, threshold: 0, source: true});
+	console.log(pendingTransactions)
+	const blockCount = await rai.blocks.count();
+	const ws = new WebSocket(`wss://localhost:${server.address().port}`, {
 		rejectUnauthorized: false
 	});
 
-
-	ws.on('open', async function open() {
-		rai.blocks.count().then((res: any) => {
-			console.log(res)
-		});
-
-		const pendingTransactions = await rai.accounts.pending({accounts: ''});
-
-		console.log(pendingTransactions);
-
-	});
+	ws.on('open', () => {
+		ws.send(blockCount.count);
+		// ws.send(pendingTransactions.blocks);
+	})
 }
 
 function startServer() {
